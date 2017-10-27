16c9af3a8a8d529fbd7d46870d41b53fe4f82231
Thibault Kestens <thibault.kestens@cegeka.com>
2017-10-27 15:57:21 +0200

    created BookService and BookRepository

--- src/main/java/be/cegeka/bibliothouris/domain/books/BookRepository.java
+++ src/main/java/be/cegeka/bibliothouris/domain/books/BookRepository.java
@@ -1,4 +1,19 @@
 package be.cegeka.bibliothouris.domain.books;
 
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Named
 public class BookRepository {
+
+    private List<Book> books = new ArrayList<>();
+
+    public List<Book> getAllBooks(){
+        return books;
+    }
+    public void addBook(Book book){
+        books.add(book);
+    }
 }
--- src/main/java/be/cegeka/bibliothouris/domain/books/BookService.java
+++ src/main/java/be/cegeka/bibliothouris/domain/books/BookService.java
@@ -1,4 +1,20 @@
 package be.cegeka.bibliothouris.domain.books;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named
 public class BookService {
+
+    @Inject
+    private BookRepository bookRepository;
+
+    public void addBook(String bookTitle, String authorLastName, String authorFirstName, String iSBN) {
+        bookRepository.addBook(new Book(bookTitle, authorLastName, authorFirstName, iSBN));
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.getAllBooks();
+    }
 }
