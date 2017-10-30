dde8007d15c5b6beac63b03acc7edfa1cd88b5f7
Thibault Kestens <thibault.kestens@cegeka.com>
2017-10-30 16:06:32 +0100

    formatting changes

--- src/main/java/be/cegeka/bibliothouris/domain/books/BookRepository.java
+++ src/main/java/be/cegeka/bibliothouris/domain/books/BookRepository.java
@@ -12,17 +12,18 @@ public class BookRepository {
 
     private List<Book> books = new ArrayList<>();
 
-    public List<Book> getAllBooks(){
+    public List<Book> getAllBooks() {
         return books;
     }
-    public void addBook(Book book){
+
+    public void addBook(Book book) {
         books.add(book);
     }
 
     public List<Book> getOnebOokBasedOnISBN(String isbn) {
-        List<Book> singleBookList=new ArrayList<>();
-        for (Book book:books) {
-            if (book.getISBN().equals(isbn)){
+        List<Book> singleBookList = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getISBN().equals(isbn)) {
                 singleBookList.add(book);
             }
         }
@@ -31,50 +32,46 @@ public class BookRepository {
 
     public List<Book> getAllBooksByPartlyIsbn(String isbnPart) {
 
-        List<Book> possibleMatches=new ArrayList<>();
+        List<Book> possibleMatches = new ArrayList<>();
         Pattern pattern = Pattern.compile(isbnPart.toLowerCase());
-        for(Book book : books){
-           Matcher matcher =  pattern.matcher(book.getISBN().toLowerCase());
-            if(matcher.find()){
+        for (Book book : books) {
+            Matcher matcher = pattern.matcher(book.getISBN().toLowerCase());
+            if (matcher.find()) {
                 possibleMatches.add(book);
             }
         }
-
         return possibleMatches;
     }
 
     public List<Book> getAllBooksByPartlyTitle(String titlePart) {
 
-        List<Book> possibleMatches=new ArrayList<>();
+        List<Book> possibleMatches = new ArrayList<>();
         Pattern pattern = Pattern.compile(titlePart.toLowerCase());
-        for(Book book : books){
-           Matcher matcher =  pattern.matcher(book.getTitle().toLowerCase());
-            if(matcher.find()){
+        for (Book book : books) {
+            Matcher matcher = pattern.matcher(book.getTitle().toLowerCase());
+            if (matcher.find()) {
                 possibleMatches.add(book);
             }
         }
-
         return possibleMatches;
     }
 
     public List<Book> getAllBooksByPartlyAuthor(String authorPart) {
 
-        List<Book> possibleMatches=new ArrayList<>();
+        List<Book> possibleMatches = new ArrayList<>();
         Pattern pattern = Pattern.compile(authorPart.toLowerCase());
-        for(Book book : books){
-           Matcher matcher =  pattern.matcher(book.getAuthorFirstName().toLowerCase());
-            if(matcher.find()){
-                possibleMatches.add(book);
-            }
-        }for(Book book : books){
-           Matcher matcher =  pattern.matcher(book.getAuthorLastName().toLowerCase());
-            if(matcher.find()){
+        for (Book book : books) {
+            Matcher matcher = pattern.matcher(book.getAuthorFirstName().toLowerCase());
+            if (matcher.find()) {
                 possibleMatches.add(book);
             }
         }
-
+        for (Book book : books) {
+            Matcher matcher = pattern.matcher(book.getAuthorLastName().toLowerCase());
+            if (matcher.find()) {
+                possibleMatches.add(book);
+            }
+        }
         return possibleMatches;
     }
-
-
 }
